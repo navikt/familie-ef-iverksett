f67430e6c609ab1b7bf4ae26aa1162b59007a783
trondil <56258085+throndi@users.noreply.github.com>
2021-04-16 09:49:02 +0200

    Enkel testrespons

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/IverksettController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/IverksettController.kt
@@ -1,18 +1,17 @@
 package no.nav.familie.ef.iverksett.infrastruktur
 
+import no.nav.familie.ef.iverksett.infrastruktur.json.VedtakJSON
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api"])
 class IverksettController {
 
-    @RequestMapping(path = ["/test"], method = [RequestMethod.GET])
-    fun test(): ResponseEntity<String> {
-        return ResponseEntity<String>("Hei", HttpStatus.OK)
+    @PostMapping(path = ["/test"])
+    fun test(@RequestBody vedtakJSON: VedtakJSON): ResponseEntity<String> {
+        return ResponseEntity<String>(vedtakJSON.toString(), HttpStatus.OK)
     }
 
 }
