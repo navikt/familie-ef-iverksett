50e4c6e1c87f65eecad2e6b2b6fe76171eff4f25
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-04-21 09:16:42 +0200

    Aktiverer AzureAd i nais.yaml

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/IverksettController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/IverksettController.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.iverksett.infrastruktur
 import no.nav.familie.ef.iverksett.infrastruktur.json.VedtakJSON
 import no.nav.familie.ef.iverksett.infrastruktur.json.transform
 import no.nav.familie.ef.iverksett.mottak.tjeneste.MottakService
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/api"])
+@ProtectedWithClaims(issuer = "azuread")
 class IverksettController(val mottakService: MottakService) {
 
     @PostMapping(path = ["/test"])
