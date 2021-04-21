c45be2f8e5aa744ac908b0dbd166630fb049fbaf
trondil <56258085+throndi@users.noreply.github.com>
2021-04-21 18:37:11 +0200

    Spring Boot versjon. Retter på test

--- src/test/kotlin/no/nav/familie/ef/iverksett/infrastruktur/json/IverksettJsonTransformTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/infrastruktur/json/IverksettJsonTransformTest.kt
@@ -1,20 +1,16 @@
 package no.nav.familie.ef.iverksett.infrastruktur.json
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.familie.ef.iverksett.ResourceLoaderTestUtil
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class IverksettJsonTransformTest {
 
-    @Autowired
-    var mapper: ObjectMapper? = null
+    var mapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule()).registerModule(JavaTimeModule())
 
     @Test
     fun `deserialiser JSON til IverksettJson, kall toDomain for så å kalle toJson, forvent like IverksettJson`() {
