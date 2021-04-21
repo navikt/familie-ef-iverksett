b4f1797d1ee6447b6d972ccd4116bfb4961ef480
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-04-21 17:05:37 +0200

    Legger til jwt issuer i application.yml

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/ApplicationConfig.kt
@@ -1,14 +1,40 @@
 package no.nav.familie.ef.iverksett.infrastruktur.configuration
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import no.nav.familie.log.filter.LogFilter
+import no.nav.familie.log.filter.RequestTimeFilter
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
 class ApplicationConfig {
 
     @Bean
     fun kotlinModule(): KotlinModule = KotlinModule()
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        logger.info("Registering LogFilter filter")
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    @Bean
+    fun requestTimeFilter(): FilterRegistrationBean<RequestTimeFilter> {
+        logger.info("Registering RequestTimeFilter filter")
+        val filterRegistration = FilterRegistrationBean<RequestTimeFilter>()
+        filterRegistration.filter = RequestTimeFilter()
+        filterRegistration.order = 2
+        return filterRegistration
+    }
 }
