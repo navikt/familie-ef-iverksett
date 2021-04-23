6c1c65875845e6d7fb35b7a83fe0ca37b65e6217
trondil <56258085+throndi@users.noreply.github.com>
2021-04-23 13:49:48 +0200

    Squashme

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/healthcheck/HelsesjekkController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/healthcheck/HelsesjekkController.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.iverksett.infrastruktur.healthcheck
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -8,41 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
 @RestController
-class HelsesjekkController {
-
-    private val logger: Logger by lazy { LoggerFactory.getLogger(HelsesjekkController::class.java) }
+@Unprotected
+class PingController {
 
     @GetMapping("/ping")
     fun ping(): ResponseEntity<Unit> {
         return ResponseEntity.ok().build()
     }
-
-    @GetMapping("/internal/selftest")
-    fun selftest(): SelftestResult {
-        logger.debug("selftest passed")
-        return SelftestResult(aggregateResult = 0, checks = null)
-    }
-
-    @GetMapping("/internal/isAlive")
-    fun isalive(): ResponseEntity<String> {
-        return ResponseEntity.ok("Is alive")
-    }
-
-    @GetMapping("/internal/isReady")
-    fun isready(): ResponseEntity<String> {
-        return ResponseEntity.ok("Is ready")
-    }
-
-    class SelftestResult(
-            val timestamp: Instant = Instant.now(),
-            val aggregateResult: Int,
-            val checks: List<Check>?
-    )
-
-    class Check(
-            val endpoint: String,
-            val description: String,
-            val errorMessage: String,
-            val result: Int
-    )
 }
