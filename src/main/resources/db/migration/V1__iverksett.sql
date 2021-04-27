4b4c4a4097dedd698456402dc721961b50a7458c
trondil <56258085+throndi@users.noreply.github.com>
2021-04-27 23:37:09 +0200

    Legger til flere tableutkast

--- src/main/resources/db/migration/V1__iverksett.sql
+++ src/main/resources/db/migration/V1__iverksett.sql
@@ -1,49 +1,89 @@
 CREATE TABLE Iverksett
 (
     id                                       UUID PRIMARY KEY,
-    forrigeTilkjentYtelse                    VARCHAR,---?---
-    tilkjentYtelse                           VARCHAR,---?---
-    fagsakId                                 VARCHAR,
-    saksnummer                               VARCHAR,
-    behanlingid                              VARCHAR,
-    relatertBehandlingId                     VARCHAR,
-    kode6eller7                              BOOLEAN,
-    tidspunktVedtak                          DATE,
-    vilkårsvurderinger                       VARCHAR,---?---
-    person_personIdent                       VARCHAR,
-    person_aktorId                           VARCHAR,
-    barn                                     VARCHAR, ---?---
-    behandlingType                           VARCHAR,
-    behandlingÅrsak                          VARCHAR,
-    behandlingResultat                       VARCHAR,
-    vedtak                                   VARCHAR,
-    opphørÅrsak                              VARCHAR,
-    inntekt                                  VARCHAR,--?--
-    inntektsReduksjon                        VARCHAR, ---?--
-    aktivitetskrav_aktivitetspliktInntreffer DATE,
-    aktivitetskrav_harSagtOppArbeidsforhold  BOOLEAN,
-    funksjonellId                            VARCHAR
-)
+    fagsakId                                 varchar,
+    saksnummer                               varchar,
+    behanglingId                              varchar,
+    relatertBehandlingId                     varchar,
+    kode6eller7                              boolean,
+    tidspunktVedtak                          date,
+    person_personIdent                       varchar,
+    person_aktorId                           varchar,
+    behandlingType                           varchar,
+    behandlingArsak                          varchar,
+    behandlingResultat                       varchar,
+    vedtak                                   varchar,
+    opphorArsak                              varchar,
+    aktivitetskrav_aktivitetspliktInntreffer date,
+    aktivitetskrav_harSagtOppArbeidsforhold  boolean,
+    funksjonellId                            varchar
+);
 
 CREATE TABLE Brev
 (
+    id                     UUID not null primary key,
     iverksett_id           UUID REFERENCES Iverksett (id),
-    journalpostId          VARCHAR,
-    brevdata_mottaker      VARCHAR,
-    brevdata_saksbehandler VARCHAR,
+    journalpostId          varchar,
+    brevdata_mottaker      varchar,
+    brevdata_saksbehandler varchar,
     brevdata_pdf           BYTEA
-)
+);
 
 CREATE TABLE AndelTilkjentYtelse
 (
-    periodebeløp_utbetaltPerPeriode NUMBER,
-    periodebeløp_periodetype        VARCHAR,
+    id                              UUID not null primary key,
+    iverksett_id                    UUID REFERENCES Iverksett (id),
+    forrigeYtelse                   boolean,
+    periodebeløp_utbetaltPerPeriode numeric(19,2),
+    periodebeløp_periodetype        varchar,
     periodebeløp_fraOgMed           DATE,
     periodebeløp_tilOgMed           DATE,
-    personIdent                     VARCHAR,
-    periodeId                       BIGINT,
-    forrigePeriodeId                BIGINT,
-    stønadsType                     VARCHAR,
+    personIdent                     varchar,
+    periodeId                       bigint,
+    forrigePeriodeId                bigint,
+    stønadsType                     varchar
+);
 
+create table Vilkarsvurderinger
+(
+    id                              UUID not null primary key,
+    iverksett_id                    UUID REFERENCES Iverksett (id),
+    vilkårType                      varchar,
+    resultat                        varchar
+);
 
-)
+create table Delvilkarsvurdering
+(
+    id                              UUID not null primary key,
+    vilkarsvurdering_id             UUID REFERENCES Vilkarsvurderinger (id),
+    resultat                        varchar
+
+);
+
+create table Vurdering
+(
+    id                              UUID not null primary key,
+    delvilkarsvurdering_id          UUID REFERENCES Delvilkarsvurdering (id),
+    regelId                         varchar not null,
+    svar                            varchar,
+    begrunnelse                     varchar
+);
+create table Barn
+(
+    id                              UUID not null primary key,
+    iverksett_id                    UUID REFERENCES Iverksett (id),
+    personIdent                     varchar,
+    aktorId                         varchar
+);
+
+create table Inntekt
+(
+    id                              bigint not null primary key,
+    iverksett_id                    UUID REFERENCES Iverksett (id),
+    utbetaltPerPeriode              bigint,
+    periodeType                     varchar,
+    fraOgMed                        date,
+    tilOgMed                        date,
+    inntektstype                    varchar
+);
+
