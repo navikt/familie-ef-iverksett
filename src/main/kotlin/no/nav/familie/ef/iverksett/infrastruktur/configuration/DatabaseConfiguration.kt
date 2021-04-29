5a9c6f04e8d2ee8571ba7d6c90d929881c891c6a
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-04-29 11:06:57 +0200

    Satt opp db lokalt og tatt i bruk task-rammeverket

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/DatabaseConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/DatabaseConfiguration.kt
@@ -1,16 +1,30 @@
 package no.nav.familie.ef.iverksett.infrastruktur.configuration
 
+import no.nav.familie.prosessering.PropertiesWrapperTilStringConverter
+import no.nav.familie.prosessering.StringTilPropertiesWrapperConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.transaction.PlatformTransactionManager
 import javax.sql.DataSource
 
 @Configuration
-class DatabaseConfiguration {
+@EnableJdbcRepositories("no.nav.familie")
+class DatabaseConfiguration : AbstractJdbcConfiguration() {
 
     @Bean
     fun transactionManager(dataSource: DataSource): PlatformTransactionManager {
         return DataSourceTransactionManager(dataSource)
+    }
+
+    @Bean
+    override fun jdbcCustomConversions(): JdbcCustomConversions {
+        return JdbcCustomConversions(listOf(
+            PropertiesWrapperTilStringConverter(),
+            StringTilPropertiesWrapperConverter()
+        ))
     }
 }
