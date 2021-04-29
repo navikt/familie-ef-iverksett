98fd2765feb32ee5af1a0a1c854fdb55bc2836a0
trondil <56258085+throndi@users.noreply.github.com>
2021-04-29 09:16:50 +0200

    Legger til en TODO

--- src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettConfiguration.kt
@@ -3,18 +3,18 @@ package no.nav.familie.ef.iverksett.lagreIverksett.infrastruktur
 import no.nav.familie.ef.iverksett.lagreIverksett.tjeneste.LagreIverksettService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 @Configuration
 class LagreIverksettConfiguration {
 
     @Bean
-    fun lagreIverksettJdbc(namedParameterJdbcTemplate: NamedParameterJdbcTemplate) : LagreIverksettJdbc {
+    fun lagreIverksettJdbc(namedParameterJdbcTemplate: NamedParameterJdbcTemplate): LagreIverksettJdbc {
         return LagreIverksettJdbc(namedParameterJdbcTemplate)
     }
+
     @Bean
-    fun lagreIverksettService(lagreIverksettJdbc: LagreIverksettJdbc) : LagreIverksettService {
+    fun lagreIverksettService(lagreIverksettJdbc: LagreIverksettJdbc): LagreIverksettService {
         return LagreIverksettService(lagreIverksettJdbc)
     }
 }
--- src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettJdbc.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettJdbc.kt
@@ -3,28 +3,37 @@ package no.nav.familie.ef.iverksett.lagreIverksett.infrastruktur
 import arrow.core.Either
 import no.nav.familie.ef.iverksett.lagreIverksett.tjeneste.KunneIkkeLagreIverksett
 import no.nav.familie.ef.iverksett.lagreIverksett.tjeneste.LagreIverksett
-import org.springframework.jdbc.core.JdbcTemplate
+import org.slf4j.LoggerFactory
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import java.util.*
 
 class LagreIverksettJdbc(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate) : LagreIverksett {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun lagre(iverksettJson: String): Either<KunneIkkeLagreIverksett, Int> {
         try {
             return Either.Right(lagreIverksett(iverksettJson))
         } catch (exception: Exception) {
+            logger.error("Kunne ikke lagre iverksett json : ${exception}")
             return Either.Left(KunneIkkeLagreIverksett)
         }
     }
 
     private fun lagreIverksett(json: String): Int {
         val sql = "insert into iverksett values(:uuid, to_json(:json::json), '1.0')"
-        var mapSqlParameterSource = MapSqlParameterSource(mapOf(
-            "uuid" to UUID.randomUUID(),
-            "json" to json
-        ))
+        var mapSqlParameterSource = MapSqlParameterSource(
+            mapOf(
+                "uuid" to UUID.randomUUID(),
+                "json" to json
+            )
+        )
         return namedParameterJdbcTemplate.update(sql, mapSqlParameterSource)
     }
+
+    /**
+     * TODO : Legge på en til alle fra iverksett til brev (og en transactional)
+     */
 
 }
