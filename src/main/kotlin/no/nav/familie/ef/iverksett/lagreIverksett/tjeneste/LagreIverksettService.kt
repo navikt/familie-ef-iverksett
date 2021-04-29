310e4616a6c387ed866e998829740b04404de0e0
trondil <56258085+throndi@users.noreply.github.com>
2021-04-29 09:20:09 +0200

    Retter på en variabeldeklarasjon

--- src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettService.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettService.kt
@@ -3,12 +3,12 @@ package no.nav.familie.ef.iverksett.lagreIverksett.tjeneste
 import arrow.core.Either
 import org.slf4j.LoggerFactory
 
-class LagreIverksettService(val lagreBehandling: LagreIverksett) {
+class LagreIverksettService(val lagreIverksett: LagreIverksett) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     fun lagreIverksettJson(iverksettJson: String): Either<KunneIkkeLagreIverksett, Int> {
-        return lagreBehandling.lagre(iverksettJson).mapLeft {
+        return lagreIverksett.lagre(iverksettJson).mapLeft {
             return Either.Left(it)
         }
     }
