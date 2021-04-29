01b757b8ed95ba3c63ad43f4a1a1a0191777c108
Neringa Altanaite <neringa.altanaite@nav.no>
2021-04-29 15:19:39 +0200

    oppdatert controller

--- src/main/kotlin/no/nav/familie/ef/iverksett/startIverksett/infrastruktur/IverksettController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/startIverksett/infrastruktur/IverksettController.kt
@@ -5,28 +5,30 @@ import no.nav.familie.ef.iverksett.infrastruktur.json.IverksettJson
 import no.nav.familie.ef.iverksett.lagreIverksett.tjeneste.LagreIverksettService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping(path = ["/api"])
+@RequestMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], path = ["/api/iverksett"])
 //@ProtectedWithClaims(issuer = "azuread")
 class IverksettController(
-    val lagreIverksettService: LagreIverksettService,
-    val objectMapper: ObjectMapper
+        val lagreIverksettService: LagreIverksettService,
+        val objectMapper: ObjectMapper
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @PostMapping(value = ["/iverksett"])
-    fun iverksett(@RequestBody iverksettJson: IverksettJson) : HttpStatus {
+    @PostMapping
+    fun iverksett(@RequestPart("iverksettJson") iverksettJson: IverksettJson,
+                  @RequestPart("fil") fil: List<MultipartFile>): HttpStatus {
         lagreIverksettService.lagreIverksettJson(objectMapper.writeValueAsString(iverksettJson)).mapLeft {
-            logger.error("Kunne ikke iverksette request : ${iverksettJson}")
+            logger.error("Kunne ikke iverksette request : $iverksettJson")
             return HttpStatus.INTERNAL_SERVER_ERROR
         }
         return HttpStatus.OK
     }
-
 }
