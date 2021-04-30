4c064e955b8d116d625f0d0585c2602f3947c1c1
trondil <56258085+throndi@users.noreply.github.com>
2021-04-30 12:20:11 +0200

    Retter på no.nav.security.jwt.issuer konfigurasjon fra .aad til .azureread

--- src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettJdbc.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/infrastruktur/LagreIverksettJdbc.kt
@@ -23,7 +23,7 @@ open class LagreIverksettJdbc(val namedParameterJdbcTemplate: NamedParameterJdbc
     }
 
     private fun lagreIverksett(behandlingsId: UUID, json: String, brev: List<Brev>) {
-        val sql = "insert into iverksett values(:behandlingsId, to_json(:json::json))"
+        val sql = "insert into iverksett values(:behandlingsId, :json)"
         var mapSqlParameterSource = MapSqlParameterSource(
             mapOf(
                 "behandlingsId" to behandlingsId,
