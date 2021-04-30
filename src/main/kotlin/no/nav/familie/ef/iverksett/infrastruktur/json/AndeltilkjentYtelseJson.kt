54bb8389528aa8757e41f77369accabb54a55991
Neringa Altanaite <neringa.altanaite@nav.no>
2021-04-30 10:31:50 +0200

    oppdatert test og AndeltilkjentYtelseJson

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/json/AndeltilkjentYtelseJson.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/json/AndeltilkjentYtelseJson.kt
@@ -3,19 +3,22 @@ package no.nav.familie.ef.iverksett.infrastruktur.json
 import no.nav.familie.ef.iverksett.domene.AndelTilkjentYtelse
 import no.nav.familie.ef.iverksett.domene.Periodebeløp
 import no.nav.familie.kontrakter.ef.felles.StønadType
+import java.util.*
 
 class AndeltilkjentYtelseJson(
-    val periodebeløp: Periodebeløp,
-    val personIdent: String,
-    val periodeId: Long? = null,
-    val forrigePeriodeId: Long? = null,
-    val stønadsType: StønadType)
+        val periodebeløp: Periodebeløp,
+        val personIdent: String,
+        val periodeId: Long? = null,
+        val forrigePeriodeId: Long? = null,
+        val stønadsType: StønadType,
+        val kildeBehandlingId: UUID? = null)
 
-fun AndeltilkjentYtelseJson.toDomain() : AndelTilkjentYtelse {
+fun AndeltilkjentYtelseJson.toDomain(): AndelTilkjentYtelse {
     return AndelTilkjentYtelse(
-        this.periodebeløp,
-        this.personIdent,
-        this.periodeId,
-        this.forrigePeriodeId,
-        this.stønadsType)
+            this.periodebeløp,
+            this.personIdent,
+            this.periodeId,
+            this.forrigePeriodeId,
+            this.stønadsType,
+            this.kildeBehandlingId)
 }
