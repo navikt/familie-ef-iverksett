1f31f74aa242f930795b45ca12ab36cc4810e5aa
Neringa Altanaite <neringa.altanaite@nav.no>
2021-05-04 11:12:01 +0200

    oppdatering etter QA

--- src/test/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettServiceTest.kt
@@ -4,21 +4,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.familie.ef.iverksett.ServerTest
-import no.nav.familie.ef.iverksett.domene.BehandlingResultat
-import no.nav.familie.ef.iverksett.domene.BehandlingType
-import no.nav.familie.ef.iverksett.domene.BehandlingÅrsak
-import no.nav.familie.ef.iverksett.domene.OpphørÅrsak
-import no.nav.familie.ef.iverksett.domene.Vedtak
-import no.nav.familie.ef.iverksett.infrastruktur.json.AktivitetskravJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.BrevJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.BrevdataJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.IverksettJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.PersonJson
 import no.nav.familie.ef.iverksett.lagreIverksett.infrastruktur.LagreIverksettJdbc
+import no.nav.familie.ef.iverksett.util.opprettIverksettJson
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
-import java.time.OffsetDateTime
 import java.util.*
 
 class LagreIverksettServiceTest : ServerTest() {
@@ -37,7 +26,7 @@ class LagreIverksettServiceTest : ServerTest() {
     @Test
     internal fun `lagre iverksett data og brev`() {
         val behandlingId = UUID.randomUUID()
-        val iverksettJson = opprettIverksettJson(behandlingId).toString()
+        val iverksettJson = opprettIverksettJson(behandlingId.toString(), emptyList()).toString()
 
         every { lagreIverksett.lagre(any(), any(), any()) } returns Unit
 
@@ -45,32 +34,5 @@ class LagreIverksettServiceTest : ServerTest() {
                                                  iverksettJson = iverksettJson,
                                                  emptyList())
         verify(exactly = 1) { lagreIverksett.lagre(behandlingId, iverksettJson, emptyList()) }
-    }
-
-    private fun opprettIverksettJson(behandlingId: UUID): IverksettJson {
-        return IverksettJson(
-                brev = listOf(BrevJson(
-                        "1", BrevdataJson("mottaker", "saksbehandler")),
-                              BrevJson("2", BrevdataJson("mottaker", "saksbehandler"))),
-                vedtak = Vedtak.INNVILGET,
-                forrigeTilkjentYtelse = emptyList(),
-                tilkjentYtelse = emptyList(),
-                fagsakId = "1",
-                saksnummer = "1",
-                behandlingId = behandlingId.toString(),
-                eksternId = 1L,
-                relatertBehandlingId = "2",
-                kode6eller7 = false,
-                tidspunktVedtak = OffsetDateTime.now(),
-                vilkårsvurderinger = emptyList(),
-                person = PersonJson(personIdent = "12345678910", aktorId = null),
-                barn = emptyList(),
-                behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING,
-                behandlingResultat = BehandlingResultat.FERDIGSTILT,
-                opphørÅrsak = OpphørÅrsak.PERIODE_UTLØPT,
-                aktivitetskrav = AktivitetskravJson(LocalDate.now(), false),
-                funksjonellId = "0",
-                behandlingÅrsak = BehandlingÅrsak.SØKNAD
-        )
     }
 }
--- src/test/kotlin/no/nav/familie/ef/iverksett/startIverksett/infrastruktur/IverksettControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/startIverksett/infrastruktur/IverksettControllerTest.kt
@@ -1,16 +1,8 @@
 package no.nav.familie.ef.iverksett.startIverksett.infrastruktur
 
 import no.nav.familie.ef.iverksett.ServerTest
-import no.nav.familie.ef.iverksett.domene.BehandlingResultat
-import no.nav.familie.ef.iverksett.domene.BehandlingType
-import no.nav.familie.ef.iverksett.domene.BehandlingÅrsak
-import no.nav.familie.ef.iverksett.domene.OpphørÅrsak
-import no.nav.familie.ef.iverksett.domene.Vedtak
-import no.nav.familie.ef.iverksett.infrastruktur.json.AktivitetskravJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.BrevJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.BrevdataJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.IverksettJson
-import no.nav.familie.ef.iverksett.infrastruktur.json.PersonJson
+import no.nav.familie.ef.iverksett.util.opprettBrev
+import no.nav.familie.ef.iverksett.util.opprettIverksettJson
 import no.nav.familie.http.client.MultipartBuilder
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -21,12 +13,12 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import java.time.LocalDate
-import java.time.OffsetDateTime
 import java.util.*
 
 
 class IverksettControllerTest : ServerTest() {
+
+    private val behandlingId = UUID.randomUUID()
 
     @BeforeEach
     fun setUp() {
@@ -37,7 +29,9 @@ class IverksettControllerTest : ServerTest() {
 
     @Test
     internal fun `starte iverksetting gir 200 OK`() {
-        val iverksettJson = opprettIverksettJson()
+        val listMedBrev = listOf(opprettBrev("1"), opprettBrev("2"))
+
+        val iverksettJson = opprettIverksettJson(behandlingId = behandlingId.toString(), listMedBrev)
         val request = MultipartBuilder()
                 .withJson("data", iverksettJson)
                 .withByteArray("fil", "1", byteArrayOf(12))
@@ -52,7 +46,9 @@ class IverksettControllerTest : ServerTest() {
 
     @Test
     internal fun `mangler brev`() {
-        val iverksettJson = opprettIverksettJson()
+        val listMedBrev = listOf(opprettBrev("1"), opprettBrev("2"))
+
+        val iverksettJson = opprettIverksettJson(behandlingId = behandlingId.toString(), listMedBrev)
         val request = MultipartBuilder()
                 .withJson("data", iverksettJson)
                 .build()
@@ -66,7 +62,8 @@ class IverksettControllerTest : ServerTest() {
 
     @Test
     internal fun `feil filename på brev`() {
-        val iverksettJson = opprettIverksettJson()
+        val listMedBrev = listOf(opprettBrev("1"), opprettBrev("2"))
+        val iverksettJson = opprettIverksettJson(behandlingId = behandlingId.toString(), listMedBrev)
         val request = MultipartBuilder()
                 .withJson("data", iverksettJson)
                 .withByteArray("fil", "11", byteArrayOf(12))
@@ -78,32 +75,5 @@ class IverksettControllerTest : ServerTest() {
                                                                  HttpEntity(request, headers))
 
         assertThat(respons.statusCode.value()).isEqualTo(500)
-    }
-
-    private fun opprettIverksettJson(): IverksettJson {
-        return IverksettJson(
-                brev = listOf(BrevJson(
-                        "1", BrevdataJson("mottaker", "saksbehandler")),
-                              BrevJson("2", BrevdataJson("mottaker", "saksbehandler"))),
-                vedtak = Vedtak.INNVILGET,
-                forrigeTilkjentYtelse = emptyList(),
-                tilkjentYtelse = emptyList(),
-                fagsakId = "1",
-                saksnummer = "1",
-                behandlingId = UUID.randomUUID().toString(),
-                eksternId = 1L,
-                relatertBehandlingId = "2",
-                kode6eller7 = false,
-                tidspunktVedtak = OffsetDateTime.now(),
-                vilkårsvurderinger = emptyList(),
-                person = PersonJson(personIdent = "12345678910", aktorId = null),
-                barn = emptyList(),
-                behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING,
-                behandlingResultat = BehandlingResultat.FERDIGSTILT,
-                opphørÅrsak = OpphørÅrsak.PERIODE_UTLØPT,
-                aktivitetskrav = AktivitetskravJson(LocalDate.now(), false),
-                funksjonellId = "0",
-                behandlingÅrsak = BehandlingÅrsak.SØKNAD
-        )
     }
 }
