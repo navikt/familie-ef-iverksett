5c4c52362d3edcf153c4c15686afd1b47b983571
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-04 11:56:01 +0200

    Legger til resttemplates fra ef-sak

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/ApplicationConfig.kt
@@ -2,18 +2,27 @@ package no.nav.familie.ef.iverksett.infrastruktur.configuration
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.familie.http.config.RestTemplateAzure
+import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.log.filter.LogFilter
 import no.nav.familie.log.filter.RequestTimeFilter
+import no.nav.security.token.support.client.core.http.OAuth2HttpClient
+import no.nav.security.token.support.client.spring.oauth2.DefaultOAuth2HttpClient
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan
@@ -45,6 +54,32 @@ class ApplicationConfig {
         filterRegistration.filter = RequestTimeFilter()
         filterRegistration.order = 2
         return filterRegistration
+    }
+
+    /**
+     * Overskrever felles sin som bruker proxy, som ikke skal brukes på gcp
+     */
+    @Bean
+    @Primary
+    fun restTemplateBuilder(): RestTemplateBuilder {
+        val jackson2HttpMessageConverter = MappingJackson2HttpMessageConverter(objectMapper)
+        return RestTemplateBuilder()
+                .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
+                .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS))
+                .additionalMessageConverters(listOf(jackson2HttpMessageConverter) + RestTemplate().messageConverters)
+    }
+
+    /**
+     * Overskrever OAuth2HttpClient som settes opp i token-support som ikke kan få med objectMapper fra felles
+     * pga .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
+     * og [OAuth2AccessTokenResponse] som burde settes med setters, då feltnavn heter noe annet enn feltet i json
+     */
+    @Bean
+    @Primary
+    fun oAuth2HttpClient(): OAuth2HttpClient {
+        return DefaultOAuth2HttpClient(RestTemplateBuilder()
+                                               .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
+                                               .setReadTimeout(Duration.of(4, ChronoUnit.SECONDS)))
     }
 
 }
