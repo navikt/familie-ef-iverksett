998e34b7db4006239c9b66c8965d0f7c97dbd626
trondil <56258085+throndi@users.noreply.github.com>
2021-05-10 23:52:19 +0200

    Lagrer domeneobjekt til basen i stedet for json objekt

--- src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettService.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/lagreIverksett/tjeneste/LagreIverksettService.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.ef.iverksett.lagreIverksett.tjeneste
 
 import no.nav.familie.ef.iverksett.domene.Brev
+import no.nav.familie.ef.iverksett.domene.Iverksett
 import java.util.*
 
 class LagreIverksettService(val lagreIverksett: LagreIverksett) {
 
-    fun lagreIverksettJson(behandlingsId: UUID, iverksettJson: String, brev: List<Brev>) {
-        return lagreIverksett.lagre(behandlingsId, iverksettJson, brev)
+    fun lagreIverksett(behandlingsId: UUID, iverksett: Iverksett, brev: Brev) {
+        return lagreIverksett.lagre(behandlingsId, iverksett, brev)
     }
+
 }
