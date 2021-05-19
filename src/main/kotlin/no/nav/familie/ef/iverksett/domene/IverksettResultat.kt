f31bb08a28757cef342f2a99671ed8c015b283d1
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-05-19 14:59:20 +0200

    Fjerner IverksettResultat

--- src/main/kotlin/no/nav/familie/ef/iverksett/domene/IverksettResultat.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/domene/IverksettResultat.kt
@@ -1,16 +1,7 @@
 package no.nav.familie.ef.iverksett.domene
 
-import no.nav.familie.ef.iverksett.domene.TilkjentYtelse
 import no.nav.familie.kontrakter.felles.oppdrag.OppdragStatus
 import java.time.LocalDateTime
-import java.util.*
-
-data class IverksettResultat(
-    val behandlingId: UUID,
-    val tilkjentYtelseForUtbetaling: TilkjentYtelse,
-    val oppdragResultat: OppdragResultat? = null,
-    val journalpostResultat: JournalpostResultat? = null
-)
 
 data class JournalpostResultat(
     val journalpostId: String,
