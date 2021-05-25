3a876b9468aef8da98a44a848f9c3d56b2c8e942
Neringa Altanaite <neringa.altanaite@nav.no>
2021-05-25 16:36:40 +0200

    flytter grensesnittavstemming til iverksett

---
+++ src/main/kotlin/no/nav/familie/ef/iverksett/økonomi/GrensesnittavstemmingTask.kt
@@ -0,0 +1,53 @@
+package no.nav.familie.ef.iverksett.økonomi
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.kontrakter.ef.felles.StønadType
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.felles.oppdrag.GrensesnittavstemmingRequest
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+data class GrensesnittavstemmingPayload(val fraDato: LocalDate, val stønadstype: StønadType)
+
+@Service
+@TaskStepBeskrivelse(taskStepType = GrensesnittavstemmingTask.TYPE, beskrivelse = "Utfører grensesnittavstemming mot økonomi.")
+class GrensesnittavstemmingTask(val oppdragClient: OppdragClient,
+                                val taskRepository: TaskRepository) : AsyncTaskStep {
+
+    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    override fun doTask(task: Task) {
+        with(objectMapper.readValue<GrensesnittavstemmingPayload>(task.payload)) {
+            val fraTidspunkt = fraDato.atStartOfDay()
+            val tilTidspunkt = task.triggerTid.toLocalDate().atStartOfDay()
+
+            logger.info("Gjør ${task.id} $stønadstype avstemming mot oppdrag fra $fraTidspunkt til $tilTidspunkt")
+            val grensesnittavstemmingRequest = GrensesnittavstemmingRequest(fagsystem = stønadstype.tilKlassifisering(),
+                                                                            fra = fraTidspunkt,
+                                                                            til = tilTidspunkt)
+            oppdragClient.grensesnittavstemming(grensesnittavstemmingRequest)
+        }
+    }
+
+    override fun onCompletion(task: Task) {
+        val payload = objectMapper.readValue<GrensesnittavstemmingPayload>(task.payload)
+        val nesteFradato = task.triggerTid.toLocalDate()
+        opprettGrensesnittavstemmingTask(GrensesnittavstemmingDto(stønadstype = payload.stønadstype, fraDato = nesteFradato))
+    }
+
+    fun opprettGrensesnittavstemmingTask(grensesnittavstemmingDto: GrensesnittavstemmingDto) =
+            grensesnittavstemmingDto
+                    .let { it.tilTask() }
+                    .let { taskRepository.save(it) }
+
+    companion object {
+
+        const val TYPE = "utførGrensesnittavstemming"
+    }
+}
