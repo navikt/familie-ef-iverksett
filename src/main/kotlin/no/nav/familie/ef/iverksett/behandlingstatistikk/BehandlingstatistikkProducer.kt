cb349f6addc69942dafca51a06eb4b19b545f3e8
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-06-10 13:20:28 +0200

    Rettelser på infomeldinger fra copy-paste

--- src/main/kotlin/no/nav/familie/ef/iverksett/behandlingstatistikk/BehandlingstatistikkProducer.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/behandlingstatistikk/BehandlingstatistikkProducer.kt
@@ -1,8 +1,8 @@
 package no.nav.familie.ef.iverksett.behandlingstatistikk
 
-import org.slf4j.LoggerFactory
 import no.nav.familie.eksterne.kontrakter.saksstatistikk.ef.BehandlingDVH
 import no.nav.familie.kontrakter.felles.objectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service
 class BehandlingstatistikkProducer(
         private val kafkaTemplate: KafkaTemplate<String, String>
 ) {
+
     @Value("\${ENSLIG_FORSORGER_BEHANDLING_TOPIC}")
     lateinit var topic: String
 
@@ -19,17 +20,18 @@ class BehandlingstatistikkProducer(
 
     fun sendBehandling(behandlingDvh: BehandlingDVH) {
         logger.info("Sending to Kafka topic: {}", topic)
-        secureLogger.debug("Sending to Kafka topic: {}\nVedtakStatistikk: {}", topic, behandlingDvh)
+        secureLogger.debug("Sending to Kafka topic: {}\nBehandlingstatistikk: {}", topic, behandlingDvh)
         runCatching {
             kafkaTemplate.send(topic, behandlingDvh.toJson())
-            logger.info("Vedtakstatistikk sent to Kafka")
+            logger.info("Behandlingstatistikk sent to Kafka")
             secureLogger.info("$behandlingDvh sent to Kafka.")
         }.onFailure {
-            val errorMessage = "Could not send vedtak to Kafka. Check secure logs for more information."
+            val errorMessage = "Could not send behandling to Kafka. Check secure logs for more information."
             logger.error(errorMessage)
-            secureLogger.error("Could not send vedtak to Kafka", it)
+            secureLogger.error("Could not send behandling to Kafka", it)
             throw RuntimeException(errorMessage)
         }
     }
+
     private fun Any.toJson(): String = objectMapper.writeValueAsString(this)
 }
