935b02597244d7dc82edaf192fdc4723acae5662
Johan Blomgren <blommish@users.noreply.github.com>
2021-06-14 10:56:02 +0200

    Mapper Vedtaksresultat til riktige verdier

--- src/main/kotlin/no/nav/familie/ef/iverksett/arena/VedtakhendelseMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/arena/VedtakhendelseMapper.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ef.iverksett.arena
 
 import no.nav.familie.ef.iverksett.iverksetting.domene.Iverksett
+import no.nav.familie.kontrakter.ef.felles.Vedtaksresultat
 import no.nav.familie.kontrakter.felles.Behandlingstema
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,10 +11,18 @@ private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:
 fun mapIverkesttTilVedtakHendelser(iverksett: Iverksett, aktørId: String): VedtakHendelser {
     return VedtakHendelser(
             aktoerID = aktørId,
-            avslutningsstatus = iverksett.vedtak.vedtaksresultat.name.toLowerCase(),
+            avslutningsstatus = mapAvslutningsstatus(iverksett.vedtak.vedtaksresultat),
             behandlingstema = Behandlingstema.valueOf(iverksett.fagsak.stønadstype.name.toLowerCase().capitalize()).value,
             hendelsesprodusentREF = "EF",
             applikasjonSakREF = iverksett.fagsak.eksternId.toString(),
             hendelsesTidspunkt = LocalDateTime.now().format(dateTimeFormatter)
     )
+}
+
+private fun mapAvslutningsstatus(vedtaksresultat: Vedtaksresultat): String {
+    return when (vedtaksresultat) {
+        Vedtaksresultat.INNVILGET -> "innvilget"
+        Vedtaksresultat.OPPHØRT -> "opphoert"
+        else -> error("Håndterer ikke restultat $vedtaksresultat mot arena")
+    }
 }
