2ded6f655e074950a924c88595dacc65daed5666
Johan Blomgren <blommish@users.noreply.github.com>
2021-07-07 08:31:03 +0200

    Legger til kafkaconfig for å overskreve default loggingproducerlistener (#110)
    * Legger til kafkaconfig for å overskreve default loggingproducerlistener
    Setter includecontents false på logging producer listener for å ikke logge content til loggene.
    La også til ack: all sånn att vi venter til man skrevet til alle replikas
    
    * Lager en wrapper runt kafkatemplate for å være sikker på att man bruker blocked request

--- src/test/kotlin/no/nav/familie/ef/iverksett/config/KafkaTemplateMock.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/config/KafkaTemplateMock.kt
@@ -1,15 +1,12 @@
 package no.nav.familie.ef.iverksett.config
 
-import io.mockk.Runs
-import io.mockk.every
+import io.mockk.justRun
 import io.mockk.mockk
+import no.nav.familie.ef.iverksett.infrastruktur.service.KafkaProducerService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.SendResult
-import org.springframework.util.concurrent.ListenableFuture
 
 @Configuration
 @Profile("mock-kafkatemplate")
@@ -17,12 +14,9 @@ class KafkaTemplateMock {
 
     @Bean
     @Primary
-    fun kafkaTemplate(): KafkaTemplate<String, String> {
-        val kafkaTemplate = mockk<KafkaTemplate<String, String>>(relaxed = true)
-        val listenableFuture = mockk<ListenableFuture<SendResult<String, String>>>()
-        val sendResult = mockk<SendResult<String, String>>()
-        every { kafkaTemplate.send(any(), any()) } returns listenableFuture
-        every { listenableFuture.get() } returns sendResult
-        return kafkaTemplate
+    fun kafkaProducerService(): KafkaProducerService {
+        val kafkaProducer = mockk<KafkaProducerService>(relaxed = true)
+        justRun { kafkaProducer.send(any(), any()) }
+        return kafkaProducer
     }
 }
