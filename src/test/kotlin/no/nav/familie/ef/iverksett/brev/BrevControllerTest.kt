c9384cbfc249d66df1fca55096b046917f97f3dd
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-10-04 05:22:27 +0200

    BrevControllerTest

---
+++ src/test/kotlin/no/nav/familie/ef/iverksett/brev/BrevControllerTest.kt
@@ -0,0 +1,43 @@
+package no.nav.familie.ef.iverksett.brev
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ef.iverksett.ServerTest
+import no.nav.familie.ef.iverksett.util.opprettFrittståendeBrevDto
+import no.nav.familie.kontrakter.felles.dokarkiv.ArkiverDokumentResponse
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class BrevControllerTest : ServerTest() {
+
+    @Autowired
+    lateinit var taskRepository: TaskRepository
+
+    val journalpostClient = mockk<JournalpostClient>()
+    val brevController = BrevController(journalpostClient)
+
+    @BeforeEach
+    fun setUp() {
+        headers.setBearerAuth(lokalTestToken)
+    }
+
+    @Test
+    internal fun `opprett frittstående brev (brev uten behandling) gir 200 OK`() {
+
+        val journalpostId = "123456789"
+
+        every { journalpostClient.arkiverDokument(any(), any()) } returns ArkiverDokumentResponse(
+            journalpostId,
+            true)
+
+        every { journalpostClient.distribuerBrev(journalpostId) } returns "distribuerBrevResponse"
+
+        val frittståendeBrevDto = opprettFrittståendeBrevDto()
+        val response = brevController.distribuerFrittståendeBrev(frittståendeBrevDto)
+
+        Assertions.assertThat(response.statusCode.value()).isEqualTo(200)
+    }
+}
