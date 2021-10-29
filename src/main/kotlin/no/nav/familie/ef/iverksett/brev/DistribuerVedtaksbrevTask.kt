e72322e6386fec61c50b4fff340f8d132658b5ae
ma10s <mattis.janitz@nav.no>
2021-10-29 08:24:58 +0200

    Skal opprette neste task i hovedflyt etter at vedtaksbrev er distribuert (OpprettTilbakekrevingTask)

--- src/main/kotlin/no/nav/familie/ef/iverksett/brev/DistribuerVedtaksbrevTask.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/brev/DistribuerVedtaksbrevTask.kt
@@ -1,23 +1,31 @@
 package no.nav.familie.ef.iverksett.brev
 
+import no.nav.familie.ef.iverksett.infrastruktur.task.opprettNestePubliseringTask
+import no.nav.familie.ef.iverksett.infrastruktur.task.opprettNesteTask
 import no.nav.familie.ef.iverksett.iverksetting.domene.DistribuerVedtaksbrevResultat
 import no.nav.familie.ef.iverksett.iverksetting.tilstand.TilstandRepository
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
-@TaskStepBeskrivelse(taskStepType = DistribuerVedtaksbrevTask.TYPE,
-                     maxAntallFeil = 50,
-                     settTilManuellOppfølgning = true,
-                     triggerTidVedFeilISekunder = 15 * 60L,
-                     beskrivelse = "Distribuerer vedtaksbrev.")
-class DistribuerVedtaksbrevTask(private val journalpostClient: JournalpostClient,
-                                private val tilstandRepository: TilstandRepository) : AsyncTaskStep {
+@TaskStepBeskrivelse(
+    taskStepType = DistribuerVedtaksbrevTask.TYPE,
+    maxAntallFeil = 50,
+    settTilManuellOppfølgning = true,
+    triggerTidVedFeilISekunder = 15 * 60L,
+    beskrivelse = "Distribuerer vedtaksbrev."
+)
+class DistribuerVedtaksbrevTask(
+    private val journalpostClient: JournalpostClient,
+    private val tilstandRepository: TilstandRepository,
+    private val taskRepository: TaskRepository
+) : AsyncTaskStep {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
@@ -25,11 +33,18 @@ class DistribuerVedtaksbrevTask(private val journalpostClient: JournalpostClient
         val behandlingId = UUID.fromString(task.payload)
         val journalpostId = tilstandRepository.hentJournalpostResultat(behandlingId)?.journalpostId
         val bestillingId = journalpostId?.let { journalpostClient.distribuerBrev(it) }
-        tilstandRepository.oppdaterDistribuerVedtaksbrevResultat(behandlingId = behandlingId,
-                                                                 DistribuerVedtaksbrevResultat(bestillingId = bestillingId)
+        tilstandRepository.oppdaterDistribuerVedtaksbrevResultat(
+            behandlingId = behandlingId,
+            DistribuerVedtaksbrevResultat(bestillingId = bestillingId)
         )
-        logger.info("Distribuer vedtaksbrev journalpost=[${journalpostId}] " +
-                    "for behandling=[${behandlingId}] med bestillingId=[$bestillingId]")
+        logger.info(
+            "Distribuer vedtaksbrev journalpost=[${journalpostId}] " +
+                    "for behandling=[${behandlingId}] med bestillingId=[$bestillingId]"
+        )
+    }
+
+    override fun onCompletion(task: Task) {
+        taskRepository.save(task.opprettNesteTask())
     }
 
     companion object {
