cd133ca311bd68f5c1b2e3745645c246997b7046
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-11-01 23:15:25 +0100

    Legger til NOT NULL constraint etter UPDATE, siden det allerede finnes data

--- src/main/resources/db/migration/V9__ekstern_id.sql
+++ src/main/resources/db/migration/V9__ekstern_id.sql
@@ -1,5 +1,8 @@
 ALTER TABLE iverksett
-    ADD COLUMN ekstern_id BIGINT NOT NULL;
+    ADD COLUMN ekstern_id BIGINT;
 
 UPDATE iverksett
 SET ekstern_id = (data -> 'behandling' ->> 'eksternId')::BIGINT;
+
+ALTER TABLE iverksett
+    ALTER COLUMN ekstern_id SET NOT NULL;
