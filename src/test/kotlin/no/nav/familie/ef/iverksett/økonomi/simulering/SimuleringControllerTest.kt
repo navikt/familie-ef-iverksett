4b496790039ebefaea2f1db51037c22647f0105e
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-05 12:53:25 +0100

    Skal ikke simulere førstegansbehandling som savner andeler eller kun har andeler med 0 som beløp. (#223)
    Co-Authored-By: ma10s <mattis.janitz@nav.no>

--- src/test/kotlin/no/nav/familie/ef/iverksett/økonomi/simulering/SimuleringControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/økonomi/simulering/SimuleringControllerTest.kt
@@ -4,6 +4,7 @@ import no.nav.familie.ef.iverksett.ServerTest
 import no.nav.familie.ef.iverksett.beriketSimuleringsresultat
 import no.nav.familie.ef.iverksett.detaljertSimuleringResultat
 import no.nav.familie.ef.iverksett.simuleringDto
+import no.nav.familie.ef.iverksett.økonomi.lagAndelTilkjentYtelseDto
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.simulering.BeriketSimuleringsresultat
 import no.nav.familie.kontrakter.felles.simulering.DetaljertSimuleringResultat
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
+import java.time.LocalDate
 
 class SimuleringControllerTest : ServerTest() {
 
@@ -43,6 +45,38 @@ class SimuleringControllerTest : ServerTest() {
         assertThat(respons.statusCode.value()).isEqualTo(200)
         assertThat(respons.body?.status).isEqualTo(Ressurs.Status.SUKSESS)
         assertThat(respons.body?.data).isEqualTo(beriketSimuleringsresultat())
+    }
+
+    @Test
+    internal fun `simulering av førstegangsbehandling skal gi tomt svar`() {
+        val request = simuleringDto(andeler = emptyList(), forrigeBehandlingId = null)
+        val respons = restTemplate
+                .exchange<Ressurs<BeriketSimuleringsresultat>>(localhostUrl("/api/simulering/v2/"),
+                                                               HttpMethod.POST,
+                                                               HttpEntity(request, headers))
+        assertThat(respons.statusCode.value()).isEqualTo(200)
+        assertThat(respons.body?.status).isEqualTo(Ressurs.Status.SUKSESS)
+        val beriketSimuleringsresultat = lagDefaultBeriketSimuleringsresultat()
+        assertThat(respons.body?.data).isEqualTo(beriketSimuleringsresultat)
+    }
+
+    @Test
+    internal fun `simulering av førstegangsbehandling med kun 0 beløp skal gi tomt svar`() {
+        val request = simuleringDto(andeler = listOf(lagAndelTilkjentYtelseDto(beløp = 0)), forrigeBehandlingId = null)
+        val respons = restTemplate
+                .exchange<Ressurs<BeriketSimuleringsresultat>>(localhostUrl("/api/simulering/v2/"),
+                                                               HttpMethod.POST,
+                                                               HttpEntity(request, headers))
+        assertThat(respons.statusCode.value()).isEqualTo(200)
+        assertThat(respons.body?.status).isEqualTo(Ressurs.Status.SUKSESS)
+        val beriketSimuleringsresultat = lagDefaultBeriketSimuleringsresultat()
+        assertThat(respons.body?.data).isEqualTo(beriketSimuleringsresultat)
+    }
+
+    private fun lagDefaultBeriketSimuleringsresultat(): BeriketSimuleringsresultat {
+        val defaultSimuleringsresultat = DetaljertSimuleringResultat(emptyList())
+        val oppsummering = lagSimuleringsoppsummering(defaultSimuleringsresultat, LocalDate.now())
+        return BeriketSimuleringsresultat(defaultSimuleringsresultat, oppsummering)
     }
 
 
