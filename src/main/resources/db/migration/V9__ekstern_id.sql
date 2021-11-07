79cd87f7f4db207140c915a1c0313449e60a7823
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-11-07 01:15:16 +0100

    Legger til sql updates per type for ekstern_id

--- src/main/resources/db/migration/V9__ekstern_id.sql
+++ src/main/resources/db/migration/V9__ekstern_id.sql
@@ -2,7 +2,10 @@ ALTER TABLE iverksett
     ADD COLUMN ekstern_id BIGINT;
 
 UPDATE iverksett
-SET ekstern_id = (data -> 'behandling' ->> 'eksternId')::BIGINT;
+SET ekstern_id = (data -> 'behandling' ->> 'eksternId')::BIGINT WHERE type = 'VANLIG';
+
+UPDATE iverksett
+SET ekstern_id = (data -> 'tilkjentYtelseMedMetaData' ->> 'eksternBehandlingId')::BIGINT WHERE type = 'TEKNISK_OPPHØR';
 
 ALTER TABLE iverksett
     ALTER COLUMN ekstern_id SET NOT NULL;
