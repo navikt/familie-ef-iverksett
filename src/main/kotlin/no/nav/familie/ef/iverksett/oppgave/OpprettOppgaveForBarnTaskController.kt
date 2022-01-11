3d14c0624f32587126c3822de66396f8c0adfd30
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-01-11 10:54:44 +0100

    Controller som brukes av ef-sak for å sende over en liste av oppgaver for barn som skal opprettes

--- src/main/kotlin/no/nav/familie/ef/iverksett/oppgave/OpprettOppgaveForBarnTaskController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/oppgave/OpprettOppgaveForBarnTaskController.kt
@@ -1,12 +1,17 @@
 package no.nav.familie.ef.iverksett.oppgave
 
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.prosessering.domene.Task
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Properties
 
 @RestController
 @RequestMapping(
@@ -15,12 +20,22 @@ import org.springframework.web.bind.annotation.RestController
 )
 @ProtectedWithClaims(issuer = "azuread")
 class OpprettOppgaveForBarnTaskController(
-        private val opprettOppgaveForBarnTask: OpprettOppgaveForBarnTask
+        private val oppgaveForBarnTask: OpprettOppgaveForBarnTask
 ) {
 
-    @GetMapping("/opprett")
-    fun opprettTask(): ResponseEntity<Unit> {
-        opprettOppgaveForBarnTask.opprettTaskForNesteUke()
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @PostMapping("/oppgaverforbarn")
+    fun opprettTask(@RequestBody oppgaverForBarn: OppgaverForBarn): ResponseEntity<Unit> {
+        oppgaverForBarn.oppgaverForBarn.forEach {
+            try {
+                oppgaveForBarnTask.doTask(Task(OpprettOppgaveForBarnTask.TYPE,
+                                               objectMapper.writeValueAsString(it),
+                                               Properties()))
+            } catch (ex: Exception) {
+                logger.error("Kunne ikke opprette task for barn som fyller år med OppgaveForBarn=$it")
+            }
+        }
         return ResponseEntity(HttpStatus.OK)
     }
 }
