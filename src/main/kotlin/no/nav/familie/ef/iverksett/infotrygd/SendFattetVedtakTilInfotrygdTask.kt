90e5a2087831dd05c307c567209728c6bfa625cb
Johan Blomgren <blommish@users.noreply.github.com>
2022-01-24 16:09:12 +0100

    Trenger ikke å sende fattet vedtak flere ganger til Infotrygd, og unngår eventuelle feil ved å ikke gjøre det (#305)

--- src/main/kotlin/no/nav/familie/ef/iverksett/infotrygd/SendFattetVedtakTilInfotrygdTask.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infotrygd/SendFattetVedtakTilInfotrygdTask.kt
@@ -9,6 +9,7 @@ import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -23,9 +24,16 @@ class SendFattetVedtakTilInfotrygdTask(private val infotrygdFeedClient: Infotryg
                                        private val iverksettingRepository: IverksettingRepository,
                                        private val taskRepository: TaskRepository) : AsyncTaskStep {
 
+    private final val logger = LoggerFactory.getLogger(javaClass)
+
     override fun doTask(task: Task) {
         val behandlingId = UUID.fromString(task.payload)
         val iverksett = iverksettingRepository.hent(behandlingId)
+        if (iverksett.behandling.forrigeBehandlingId != null) {
+            logger.info("Sender ikke ett nytt vedtak til infotrygd for fagsak=${iverksett.fagsak.fagsakId}" +
+                        " då det allerede er sendt på forrige behandling")
+            return
+        }
 
         val stønadstype = iverksett.fagsak.stønadstype
         val personIdenter = familieIntegrasjonerClient.hentIdenter(iverksett.søker.personIdent, true)
