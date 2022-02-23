1d05ebe4653bd5ea3c94db8750be35c4dc1c1196
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-02-23 14:01:19 +0100

    Finpuss av databasescript etter endring av modell i journalpostresultat og vedtaksbrevresultat

--- src/main/resources/db/migration/V14__dropp_journalpostresultat_brevmottakere.sql
+++ src/main/resources/db/migration/V14__dropp_journalpostresultat_brevmottakere.sql
@@ -2,15 +2,17 @@ UPDATE iverksett_resultat
 SET vedtaksbrevresultat =
         CONCAT('{', journalpostresultat -> 'journalpostId',
                ':', vedtaksbrevresultat, '}')::JSON
-WHERE vedtaksbrevresultat IS NOT NULL;
+WHERE vedtaksbrevresultat IS NOT NULL
+AND journalpostresultat IS NOT NULL;
+
 
 UPDATE iverksett_resultat
 SET journalpostresultat =
-        CONCAT('{', tilkjentytelseforutbetaling -> 'utbetalingsoppdrag' -> 'aktoer',
+        CONCAT('{', i.data -> 'søker' -> 'personIdent',
                ':', journalpostresultat, '}')::JSON
+from iverksett i
 WHERE journalpostresultat IS NOT NULL
-AND tilkjentytelseforutbetaling IS NOT NULL;
-
+  AND iverksett_resultat.behandling_id = i.behandling_id;
 
 UPDATE iverksett_resultat
 SET journalpostresultat = journalpostresultatbrevmottakere
