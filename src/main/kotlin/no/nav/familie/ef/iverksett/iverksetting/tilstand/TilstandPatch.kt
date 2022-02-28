4f6b8eaee3ac9374d95b2590f688a65abe5d5e00
Johan Blomgren <blommish@users.noreply.github.com>
2022-02-28 16:44:38 +0100

    FIkser requestmapping av restcontroller

--- src/main/kotlin/no/nav/familie/ef/iverksett/iverksetting/tilstand/TilstandPatch.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/iverksetting/tilstand/TilstandPatch.kt
@@ -12,11 +12,13 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-@RestController("api/tilstandpatch")
+@RestController
+@RequestMapping("api/tilstandpatch")
 @Unprotected
 class TilstandPatchController(private val tilstandPatch: TilstandPatch) {
 
