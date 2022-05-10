772480d1072182ce2690578f74e9af1720a18f2e
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-05-10 12:47:06 +0200

    Skal ha sammenhengende perioder når vi oppdaterer tilbakekrevingsperiodene i iverksett.

--- src/main/kotlin/no/nav/familie/ef/iverksett/økonomi/simulering/SimuleringUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/økonomi/simulering/SimuleringUtil.kt
@@ -11,6 +11,7 @@ import no.nav.familie.kontrakter.felles.simulering.SimuleringMottaker
 import no.nav.familie.kontrakter.felles.simulering.Simuleringsoppsummering
 import no.nav.familie.kontrakter.felles.simulering.Simuleringsperiode
 import no.nav.familie.kontrakter.felles.simulering.SimulertPostering
+import no.nav.familie.kontrakter.felles.tilbakekreving.Periode
 import java.math.BigDecimal
 import java.math.BigDecimal.ZERO
 import java.time.LocalDate
@@ -121,4 +122,27 @@ private object SimuleringsperiodeEtterbetaling {
 fun BeriketSimuleringsresultat.harFeilutbetaling(): Boolean {
     return this.oppsummering.feilutbetaling > ZERO
 }
+
+
+fun Simuleringsoppsummering.hentSammenhengendePerioderMedFeilutbetaling(): List<Periode> {
+    val perioderMedFeilutbetaling =
+            perioder.sortedBy { it.fom }.filter { it.feilutbetaling > BigDecimal(0) }.map {
+                Periode(it.fom, it.tom)
+            }
+
+    return perioderMedFeilutbetaling.fold(mutableListOf()) { akkumulatorListe, nestePeriode ->
+        val gjeldendePeriode = akkumulatorListe.lastOrNull()
+
+        if (gjeldendePeriode != null && erPerioderSammenhengende(gjeldendePeriode, nestePeriode)) {
+            val oppdatertGjeldendePeriode = Periode(fom = gjeldendePeriode.fom,tom=nestePeriode.tom)
+            akkumulatorListe.removeLast()
+            akkumulatorListe.add(oppdatertGjeldendePeriode)
+        } else {
+            akkumulatorListe.add(nestePeriode)
+        }
+        akkumulatorListe
+    }
+}
+private fun erPerioderSammenhengende(gjeldendePeriode: Periode, nestePeriode: Periode) =
+        gjeldendePeriode.tom.plusDays(1) == nestePeriode.fom
 
