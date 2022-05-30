f0e796e286761d105d13948b59fafeee8dd26ca5
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-05-30 09:32:00 +0200

    Cucumber-test for oppdrag

---
+++ src/test/kotlin/no/nav/familie/ef/iverksett/cucumber/steps/OppdragStepDefinitions.kt
@@ -0,0 +1,34 @@
+package no.nav.familie.ef.iverksett.cucumber.steps
+
+import io.cucumber.datatable.DataTable
+import io.cucumber.java.no.Gitt
+import io.cucumber.java.no.Når
+import io.cucumber.java.no.Så
+import no.nav.familie.ef.iverksett.iverksetting.domene.TilkjentYtelse
+import no.nav.familie.ef.iverksett.iverksetting.domene.TilkjentYtelseMedMetaData
+import no.nav.familie.ef.iverksett.økonomi.utbetalingsoppdrag.UtbetalingsoppdragGenerator
+
+class OppdragStepDefinitions {
+
+    lateinit var nyTilkjentYtelse: TilkjentYtelseMedMetaData
+    lateinit var forrigeTilkjentYtelse: TilkjentYtelse
+
+    @Gitt("følgende andeler")
+    fun følgendeAndeler(dataTable: DataTable) {
+        // AndelTilkjentYtelseParser
+        // startdato = TilkjentYtelseParser.mapStartdatoer(dataTable)
+    }
+
+    @Gitt("følgende oppdrag")
+    fun følgende_oppdrag(dataTable: DataTable) {
+    }
+
+    @Når("lagTilkjentYtelseMedUtbetalingsoppdrag")
+    fun lagTilkjentYtelseMedUtbetalingsoppdrag() {
+        UtbetalingsoppdragGenerator.lagTilkjentYtelseMedUtbetalingsoppdrag(nyTilkjentYtelse, forrigeTilkjentYtelse)
+    }
+
+    @Så("forvent følgende tilkjent ytelse")
+    fun forventTilkjentYtelse() {
+    }
+}
