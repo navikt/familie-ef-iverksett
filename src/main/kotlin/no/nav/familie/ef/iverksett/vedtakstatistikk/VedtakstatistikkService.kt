20450934a6d48d7cb03244f6750ad00b0f1f52db
eirarset <eirik.arseth@nav.no>
2022-06-21 12:40:51 +0200

    Skal featuretoggle vedtaksstatistikk skolepenger

--- src/main/kotlin/no/nav/familie/ef/iverksett/vedtakstatistikk/VedtakstatistikkService.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/vedtakstatistikk/VedtakstatistikkService.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.iverksett.vedtakstatistikk
 
+import no.nav.familie.ef.iverksett.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.iverksett.iverksetting.domene.Iverksett
 import no.nav.familie.ef.iverksett.iverksetting.domene.IverksettBarnetilsyn
 import no.nav.familie.ef.iverksett.iverksetting.domene.IverksettOvergangsstønad
@@ -7,7 +8,7 @@ import no.nav.familie.ef.iverksett.iverksetting.domene.IverksettSkolepenger
 import org.springframework.stereotype.Service
 
 @Service
-class VedtakstatistikkService(private val vedtakstatistikkKafkaProducer: VedtakstatistikkKafkaProducer) {
+class VedtakstatistikkService(private val vedtakstatistikkKafkaProducer: VedtakstatistikkKafkaProducer, private val featureToggleService: FeatureToggleService) {
 
     fun sendTilKafka(iverksett: Iverksett, forrigeIverksett: Iverksett?) {
         // Kunne ikke bruke sealed class i kontrakt mot datavarehus og det blir derfor if-else her
@@ -18,8 +19,12 @@ class VedtakstatistikkService(private val vedtakstatistikkKafkaProducer: Vedtaks
             val vedtakstatistikk = VedtakstatistikkMapper.mapTilVedtakBarnetilsynDVH(iverksett, forrigeIverksett?.behandling?.eksternId)
             vedtakstatistikkKafkaProducer.sendVedtak(vedtakstatistikk)
         } else if (iverksett is IverksettSkolepenger) {
-            val vedtakstatistikk = VedtakstatistikkMapper.mapTilVedtakSkolepengeDVH(iverksett, forrigeIverksett?.behandling?.eksternId)
-            vedtakstatistikkKafkaProducer.sendVedtak(vedtakstatistikk)
+            if (featureToggleService.isEnabled("familie.ef.iverksett.vedtaksstatistikk-skolepenger")) {
+                val vedtakstatistikk = VedtakstatistikkMapper.mapTilVedtakSkolepengeDVH(iverksett, forrigeIverksett?.behandling?.eksternId)
+                vedtakstatistikkKafkaProducer.sendVedtak(vedtakstatistikk)
+            } else {
+                error("Featuretoggle \"familie.ef.iverksett.vedtaksstatistikk-skolepenger\" ikke påskrudd ")
+            }
         }
     }
 }
