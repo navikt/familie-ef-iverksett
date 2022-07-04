80ef80db9e55be8815751d2cfb695f44beccb40d
Johan Blomgren <blommish@users.noreply.github.com>
2022-07-04 11:10:28 +0200

    Fjerner teknisk opphør fra koden då den ikke er i bruk og kan ha unventet oppførsel

--- src/main/kotlin/no/nav/familie/ef/iverksett/iverksetting/domene/TekniskOpphør.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.ef.iverksett.iverksetting.domene
-
-import java.util.UUID
-
-data class TekniskOpphør(val forrigeBehandlingId: UUID, val tilkjentYtelseMedMetaData: TilkjentYtelseMedMetaData)
