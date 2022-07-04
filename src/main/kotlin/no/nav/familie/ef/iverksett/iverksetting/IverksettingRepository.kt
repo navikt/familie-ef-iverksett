7acb311342605ffa5faac274e69215292b6c9bc6
Johan Blomgren <blommish@users.noreply.github.com>
2022-07-04 15:26:04 +0200

    Fjerner type fra iverksett-tabellen då den ikke er i bruk etter at teknisk opphør er fjernet (#515)

--- src/main/kotlin/no/nav/familie/ef/iverksett/iverksetting/IverksettingRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/iverksetting/IverksettingRepository.kt
@@ -2,7 +2,6 @@ package no.nav.familie.ef.iverksett.iverksetting
 
 import no.nav.familie.ef.iverksett.iverksetting.domene.Brev
 import no.nav.familie.ef.iverksett.iverksetting.domene.Iverksett
-import no.nav.familie.ef.iverksett.iverksetting.domene.IverksettType
 import no.nav.familie.ef.iverksett.util.getUUID
 import no.nav.familie.ef.iverksett.util.queryForJson
 import no.nav.familie.kontrakter.felles.objectMapper
@@ -23,13 +22,12 @@ class IverksettingRepository(val namedParameterJdbcTemplate: NamedParameterJdbcT
     }
 
     private fun lagreIverksett(behandlingId: UUID, iverksett: Iverksett) {
-        val sql = "INSERT INTO iverksett VALUES(:behandlingId, :iverksettJson::JSON, :type, :eksternId)"
+        val sql = "INSERT INTO iverksett VALUES(:behandlingId, :iverksettJson::JSON, :eksternId)"
         val iverksettString = objectMapper.writeValueAsString(iverksett)
         val mapSqlParameterSource = MapSqlParameterSource(
             mapOf(
                 "behandlingId" to behandlingId,
                 "iverksettJson" to iverksettString,
-                "type" to IverksettType.VANLIG.name,
                 "eksternId" to iverksett.behandling.eksternId
             )
         )
@@ -49,10 +47,7 @@ class IverksettingRepository(val namedParameterJdbcTemplate: NamedParameterJdbcT
 
     fun hent(behandlingId: UUID): Iverksett {
         val mapSqlParameterSource = MapSqlParameterSource(
-            mapOf(
-                "behandlingId" to behandlingId,
-                "type" to IverksettType.VANLIG.name
-            )
+            mapOf("behandlingId" to behandlingId)
         )
         return namedParameterJdbcTemplate.queryForJson(HENT_IVERKSETT_SQL, mapSqlParameterSource)
             ?: error("Finner ikke iverksett med behandlingId=$behandlingId")
@@ -60,10 +55,7 @@ class IverksettingRepository(val namedParameterJdbcTemplate: NamedParameterJdbcT
 
     fun hentAvEksternId(eksternId: Long): Iverksett {
         val mapSqlParameterSource = MapSqlParameterSource(
-            mapOf(
-                "eksternId" to eksternId,
-                "type" to IverksettType.VANLIG.name
-            )
+            mapOf("eksternId" to eksternId,)
         )
         return namedParameterJdbcTemplate.queryForJson(HENT_IVERKSETT_EKSTERN_ID_SQL, mapSqlParameterSource)
             ?: error("Finner ikke iverksett med eksternId=$eksternId")
@@ -82,7 +74,7 @@ class IverksettingRepository(val namedParameterJdbcTemplate: NamedParameterJdbcT
     // language=PostgreSQL
     companion object {
 
-        const val HENT_IVERKSETT_SQL = "SELECT data FROM iverksett WHERE behandling_id = :behandlingId AND type = :type"
-        const val HENT_IVERKSETT_EKSTERN_ID_SQL = "SELECT data FROM iverksett WHERE ekstern_id = :eksternId AND type = :type"
+        const val HENT_IVERKSETT_SQL = "SELECT data FROM iverksett WHERE behandling_id = :behandlingId"
+        const val HENT_IVERKSETT_EKSTERN_ID_SQL = "SELECT data FROM iverksett WHERE ekstern_id = :eksternId"
     }
 }
