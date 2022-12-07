2a6f9e0a41e7f37ecb326659eed620e12b551966
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-12-07 13:30:41 +0100

    Fjerner ubrukt kode i oppgaveclient

--- src/main/kotlin/no/nav/familie/ef/iverksett/oppgave/OppgaveClient.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/oppgave/OppgaveClient.kt
@@ -3,11 +3,6 @@ package no.nav.familie.ef.iverksett.oppgave
 import no.nav.familie.ef.iverksett.util.medContentTypeJsonUTF8
 import no.nav.familie.http.client.AbstractRestClient
 import no.nav.familie.kontrakter.felles.Ressurs
-import no.nav.familie.kontrakter.felles.getDataOrThrow
-import no.nav.familie.kontrakter.felles.oppgave.FinnMappeResponseDto
-import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
-import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveResponseDto
-import no.nav.familie.kontrakter.felles.oppgave.Oppgave
 import no.nav.familie.kontrakter.felles.oppgave.OppgaveResponse
 import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgaveRequest
 import org.springframework.beans.factory.annotation.Qualifier
@@ -15,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
-import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
 @Component
@@ -26,17 +20,6 @@ class OppgaveClient(
 
     val oppgaveUrl = "$integrasjonUrl/api/oppgave"
 
-    fun hentOppgaver(finnOppgaveRequest: FinnOppgaveRequest): List<Oppgave> {
-        val opprettOppgaveUri = URI.create("$oppgaveUrl/v4")
-        val response =
-            postForEntity<Ressurs<FinnOppgaveResponseDto>>(
-                opprettOppgaveUri,
-                finnOppgaveRequest,
-                HttpHeaders().medContentTypeJsonUTF8()
-            )
-        return response.getDataOrThrow().oppgaver
-    }
-
     fun opprettOppgave(opprettOppgaveRequest: OpprettOppgaveRequest): Long? {
         val opprettOppgaveUri = URI.create("$oppgaveUrl/opprett")
         val response =
@@ -46,33 +29,5 @@ class OppgaveClient(
                 HttpHeaders().medContentTypeJsonUTF8()
             )
         return response.data?.oppgaveId
-    }
-
-    fun finnOppgaveMedId(oppgaveId: Long): Oppgave {
-        val response = getForEntity<Ressurs<Oppgave>>(
-            URI.create("$oppgaveUrl/$oppgaveId"),
-            HttpHeaders().medContentTypeJsonUTF8()
-        )
-        return response.getDataOrThrow()
-    }
-
-    fun finnMapper(enhetsnummer: String, limit: Long): FinnMappeResponseDto {
-        val response = getForEntity<Ressurs<FinnMappeResponseDto>>(
-            UriComponentsBuilder.fromUri(URI.create("$oppgaveUrl/mappe/sok"))
-                .queryParam("enhetsnr", enhetsnummer)
-                .queryParam("limit", limit)
-                .build()
-                .toUri()
-        )
-        return response.getDataOrThrow()
-    }
-
-    fun oppdaterOppgave(oppgave: Oppgave): Long {
-        val response = patchForEntity<Ressurs<OppgaveResponse>>(
-            URI.create("$oppgaveUrl/${oppgave.id!!}/oppdater"),
-            oppgave,
-            HttpHeaders().medContentTypeJsonUTF8()
-        )
-        return response.getDataOrThrow().oppgaveId
     }
 }
