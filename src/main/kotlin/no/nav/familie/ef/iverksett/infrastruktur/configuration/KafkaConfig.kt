6179333e07068616ddd55488d860c79c918a781a
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-06-02 13:28:51 +0200

    Legger inn schema registry url og auth

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/configuration/KafkaConfig.kt
@@ -1,10 +1,12 @@
 package no.nav.familie.ef.iverksett.infrastruktur.configuration
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.brukernotifikasjon.schemas.input.BeskjedInput
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,6 +16,15 @@ import org.springframework.kafka.support.LoggingProducerListener
 
 @Configuration
 class KafkaConfig {
+
+    @Value("\${KAFKA_SCHEMA_REGISTRY}")
+    lateinit var schemaRegistryUrl: String
+
+    @Value("\${KAFKA_SCHEMA_REGISTRY_USER}")
+    lateinit var schemaRegistryUser: String
+
+    @Value("\${KAFKA_SCHEMA_REGISTRY_PASSWORD}")
+    lateinit var schemaRegistryPassword: String
 
     @Bean
     fun kafkaTemplate(properties: KafkaProperties): KafkaTemplate<String, String> {
@@ -33,6 +44,9 @@ class KafkaConfig {
         val producerFactory = DefaultKafkaProducerFactory<NokkelInput, BeskjedInput>(properties.buildProducerProperties().apply {
             put(KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
             put(VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
+            put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl)
+            put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
+            put(KafkaAvroSerializerConfig.USER_INFO_CONFIG, "$schemaRegistryUser:$schemaRegistryPassword",)
         })
 
         return KafkaTemplate(producerFactory).apply<KafkaTemplate<NokkelInput, BeskjedInput>> {
