f3ec0a81c13aa562b1a3f32a7566454bdeb1b528
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-06-02 12:21:37 +0200

    Legger til grunnbeløp for 2023

--- src/main/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/Grunnbeløp.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/Grunnbeløp.kt
@@ -10,7 +10,13 @@ import java.time.format.DateTimeFormatter
 val grunnbeløpsperioder: List<Grunnbeløp> =
     listOf(
         Grunnbeløp(
-            periode = Månedsperiode(YearMonth.parse("2022-05"), YearMonth.from(LocalDate.MAX)),
+            periode = Månedsperiode(YearMonth.parse("2023-05"), YearMonth.from(LocalDate.MAX)),
+            grunnbeløp = 118_620.toBigDecimal(),
+            perMnd = 9_885.toBigDecimal(),
+            gjennomsnittPerÅr = 116_239.toBigDecimal(),
+        ),
+        Grunnbeløp(
+            periode = Månedsperiode(YearMonth.parse("2022-05"), YearMonth.parse("2023-04")),
             grunnbeløp = 111_477.toBigDecimal(),
             perMnd = 9_290.toBigDecimal(),
             gjennomsnittPerÅr = 109_784.toBigDecimal(),
