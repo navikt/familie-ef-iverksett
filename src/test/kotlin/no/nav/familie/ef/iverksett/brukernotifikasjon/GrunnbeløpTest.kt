02d0a87ba32532505e64bbb1399aff678ea39b0e
ma10s <mattis.janitz@nav.no>
2023-06-07 10:15:07 +0200

    For å unngå feil melding til bruker vil vi sende generell melding til alle (uten inntekt) ved g-omregning.

--- src/test/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/GrunnbeløpTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/GrunnbeløpTest.kt
@@ -1,27 +1,7 @@
 package no.nav.familie.ef.iverksett.brukernotifikasjon
 
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.mockkObject
-import io.mockk.runs
-import io.mockk.unmockkObject
-import io.mockk.verify
-import no.nav.familie.ef.iverksett.infrastruktur.transformer.toDomain
-import no.nav.familie.ef.iverksett.iverksetting.IverksettingRepository
-import no.nav.familie.ef.iverksett.lagIverksett
-import no.nav.familie.ef.iverksett.repository.findByIdOrThrow
-import no.nav.familie.ef.iverksett.util.mockFeatureToggleService
-import no.nav.familie.ef.iverksett.util.opprettIverksettDto
-import no.nav.familie.kontrakter.ef.felles.BehandlingÅrsak
-import no.nav.familie.prosessering.domene.Task
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
-import java.time.LocalDate
-import java.util.UUID
 
 class GrunnbeløpTest {
 
