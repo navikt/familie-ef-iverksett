49f8e9db316c650e9ebc39b279cf4b363a5b92e8
krikof-nav <kristian.kofoed@nav.no>
2025-01-24 11:29:47 +0100

    Introdusert ny metode i BrukernotifikasjonKafkaProducer som tar i bruk den nye builderen. Skrevet en test som passer på at den fungerer som forventet.

--- src/main/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/BrukernotifikasjonKafkaProducer.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/BrukernotifikasjonKafkaProducer.kt
@@ -15,10 +15,16 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.util.UUID
+import no.nav.tms.varsel.action.Produsent
+import no.nav.tms.varsel.action.Sensitivitet
+import no.nav.tms.varsel.action.Tekst
+import no.nav.tms.varsel.action.Varseltype
+import no.nav.tms.varsel.builder.VarselActionBuilder
 
 @Service
 class BrukernotifikasjonKafkaProducer(
     private val kafkaTemplate: KafkaTemplate<NokkelInput, BeskjedInput>,
+    private val migrertKafkaTemplate: KafkaTemplate<String, String>,
 ) {
     @Value("\${KAFKA_TOPIC_DITTNAV}")
     private lateinit var topic: String
@@ -45,6 +51,47 @@ class BrukernotifikasjonKafkaProducer(
         }
     }
 
+    fun sendBeskjedTilBrukerMedBuilder(
+        personIdent: String,
+        iverksettOvergangsstønad: IverksettOvergangsstønad,
+        behandlingId: UUID
+    ) {
+        val generertVarselId = behandlingId.toString()
+
+        val opprettVarsel = VarselActionBuilder.opprett {
+            type = Varseltype.Beskjed
+            varselId = generertVarselId
+            sensitivitet = Sensitivitet.High
+            ident = personIdent
+            aktivFremTil = null
+
+            tekst = Tekst(
+                spraakkode = "nb",
+                tekst = lagMelding(iverksettOvergangsstønad),
+                default = true,
+            )
+
+            produsent = Produsent(
+                cluster = APPLICATION_CLUSTER,
+                namespace = APPLICATION_NAMESPACE,
+                appnavn = APPLICATION_NAME,
+            )
+        }
+
+        secureLogger.info("Sender til Kafka topic: {}: {}", topic, opprettVarsel)
+
+        runCatching {
+            val producerRecord = ProducerRecord(topic, generertVarselId, opprettVarsel)
+            migrertKafkaTemplate.send(producerRecord).get()
+        }.onFailure {
+            val errorMessage = "Kunne ikke sende brukernotifikasjon til topic: $topic. Se secure logs for mer informasjon."
+            logger.error(errorMessage)
+            secureLogger.error("Kunne ikke sende brukernotifikasjon til topic: {}", topic, it)
+
+            throw RuntimeException(errorMessage)
+        }
+    }
+
     private fun lagNøkkel(
         fnr: String,
         behandlingId: UUID,
@@ -67,11 +114,18 @@ class BrukernotifikasjonKafkaProducer(
 
         return builder.build()
     }
-}
 
-fun lagMelding(iverksett: IverksettOvergangsstønad): String =
-    iverksett.vedtak.grunnbeløp?.let {
-        """Fra ${it.periode.fomDato.norskFormat()} har folketrygdens grunnbeløp økt til ${it.grunnbeløp} kroner og overgangsstønaden din er derfor endret. Se nav.no/minside for detaljer.""".trimIndent()
-    } ?: throw IllegalStateException("Mangler grunnbeløp")
+    fun lagMelding(iverksett: IverksettOvergangsstønad): String =
+        iverksett.vedtak.grunnbeløp?.let {
+            """Fra ${it.periode.fomDato.norskFormat()} har folketrygdens grunnbeløp økt til ${it.grunnbeløp} kroner og overgangsstønaden din er derfor endret. Se nav.no/minside for detaljer.""".trimIndent()
+        } ?: throw IllegalStateException("Mangler grunnbeløp")
+
+    companion object {
+        // TODO: Disse burde kanskje ligge i Application.yml filene, men lar de være her inntil videre.
+        const val APPLICATION_NAME = "familie-ef-iverksett"
+        const val APPLICATION_NAMESPACE = "teamfamilie"
+        const val APPLICATION_CLUSTER = "local"
+    }
+}
 
 private fun LocalDate.norskFormat() = this.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
