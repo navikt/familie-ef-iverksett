d393b70b9076418d35354aff47d24532c8b2c9e5
krikof-nav <kristian.kofoed@nav.no>
2025-01-27 14:48:36 +0100

    Fjernet feature toggle for ny brukernotifikasjon, samt bakover kompatibilitet. Endret tester slik at de passer den nye implementasjonen for sendBeskjedTilBruker. Slettet gammle metoder. sendBeskjedTilBruker tar nå inn melding fra utsiden, der vi nå genererer melding.

--- src/test/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/SendBrukernotifikasjonVedGOmregningTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/iverksett/brukernotifikasjon/SendBrukernotifikasjonVedGOmregningTaskTest.kt
@@ -9,7 +9,6 @@ import no.nav.familie.ef.iverksett.infrastruktur.transformer.toDomain
 import no.nav.familie.ef.iverksett.iverksetting.IverksettingRepository
 import no.nav.familie.ef.iverksett.lagIverksett
 import no.nav.familie.ef.iverksett.repository.findByIdOrThrow
-import no.nav.familie.ef.iverksett.util.mockFeatureToggleService
 import no.nav.familie.ef.iverksett.util.opprettIverksettDto
 import no.nav.familie.kontrakter.ef.felles.BehandlingÅrsak
 import no.nav.familie.prosessering.domene.Task
@@ -23,13 +22,11 @@ class SendBrukernotifikasjonVedGOmregningTaskTest {
     private val iverksettingRepository = mockk<IverksettingRepository>()
     private val brukernotifikasjonKafkaProducer = mockk<BrukernotifikasjonKafkaProducer>()
     private val taskService = mockk<TaskService>()
-    private val featureToggleService = mockFeatureToggleService()
     private val task =
         SendBrukernotifikasjonVedGOmregningTask(
             brukernotifikasjonKafkaProducer = brukernotifikasjonKafkaProducer,
             iverksettingRepository = iverksettingRepository,
             taskService = taskService,
-            featureToggleService = featureToggleService,
         )
 
     @BeforeEach
@@ -40,28 +37,17 @@ class SendBrukernotifikasjonVedGOmregningTaskTest {
                     opprettIverksettDto(behandlingId = UUID.randomUUID(), behandlingÅrsak = BehandlingÅrsak.G_OMREGNING).toDomain(),
                 ),
             )
-        every { brukernotifikasjonKafkaProducer.sendBeskjedTilBruker(any(), any()) } just runs
-        every { brukernotifikasjonKafkaProducer.sendBeskjedTilBrukerMedKotlinBuilder(any(), any(), any()) } just runs
+        every { brukernotifikasjonKafkaProducer.lagMelding(any()) } returns "test-tekst"
+        every { brukernotifikasjonKafkaProducer.sendBeskjedTilBruker(any(), any(), any(), any()) } just runs
     }
 
     @Nested
     inner class DoTaskTest {
         @Test
         fun `doTask - skal publisere vedtak til kafka`() {
-            every { featureToggleService.isEnabled(any()) } returns false
-
             task.doTask(Task(SendBrukernotifikasjonVedGOmregningTask.TYPE, UUID.randomUUID().toString()))
 
-            verify(exactly = 1) { brukernotifikasjonKafkaProducer.sendBeskjedTilBruker(any(), any()) }
-            verify(exactly = 0) { brukernotifikasjonKafkaProducer.sendBeskjedTilBrukerMedKotlinBuilder(any(), any(), any()) }
-        }
-
-        @Test
-        fun `doTask - skal publisere vedtak til kafka med Kotlin builder`() {
-            task.doTask(Task(SendBrukernotifikasjonVedGOmregningTask.TYPE, UUID.randomUUID().toString()))
-
-            verify(exactly = 0) { brukernotifikasjonKafkaProducer.sendBeskjedTilBruker(any(), any()) }
-            verify(exactly = 1) { brukernotifikasjonKafkaProducer.sendBeskjedTilBrukerMedKotlinBuilder(any(), any(), any()) }
+            verify(exactly = 1) { brukernotifikasjonKafkaProducer.sendBeskjedTilBruker(any(), any(), any(), any()) }
         }
     }
 }
