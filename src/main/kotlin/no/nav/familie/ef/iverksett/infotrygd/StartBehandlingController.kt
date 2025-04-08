b71368d737fcb4cd2c9881b5a25241d730f6616f
ma10s <mattis.janitz@nav.no>
2025-04-08 12:42:45 +0200

    Vi trenger ikke lenger si ifra til infotrygd når vi starter, eller fatter vedtak på en infotrygdsak. Vi må fortsatt sende perioder for at småbarnstilleg skal bli riktig for BA.

--- src/main/kotlin/no/nav/familie/ef/iverksett/infotrygd/StartBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infotrygd/StartBehandlingController.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.iverksett.infotrygd
 
 import no.nav.familie.kontrakter.ef.infotrygd.OpprettStartBehandlingHendelseDto
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/start-behandling")
 @ProtectedWithClaims(issuer = "azuread")
-class StartBehandlingController(
-    private val infotrygdFeedClient: InfotrygdFeedClient,
-) {
+class StartBehandlingController {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @PostMapping
+    @Deprecated("Ikke lenger i bruk etter at rutine i infotrygd ble skrudd av")
     fun startBehandling(
         @RequestBody request: OpprettStartBehandlingHendelseDto,
     ) {
-        infotrygdFeedClient.opprettStartBehandlingHendelse(request)
+        logger.warn("Deprecated - Ikke lenger i bruk etter at rutine i infotrygd ble skrudd av")
     }
 }
