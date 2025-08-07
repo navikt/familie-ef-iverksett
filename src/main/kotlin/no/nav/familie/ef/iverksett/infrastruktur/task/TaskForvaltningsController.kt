f4aae12426a206705557d15d11ebee0ffabfe637
ma10s <mattis.janitz@nav.no>
2025-08-07 12:43:51 +0200

    Vil legge til TaskForvaltningService og en test av kopieringsfunksjonalitet

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/task/TaskForvaltningsController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/task/TaskForvaltningsController.kt
@@ -1,20 +1,41 @@
 package no.nav.familie.ef.iverksett.infrastruktur.task
 
-import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Status
+import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.internal.TaskService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
-import kotlin.reflect.full.findAnnotation
+
+@Service
+class TaskForvaltningService(
+    private val taskService: TaskService,
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    @Transactional
+    fun kopierTask(task: Task): Task {
+        check(task.status == Status.MANUELL_OPPFØLGING) { "Task som skal kopieres må ha status MANUELL_OPPFØLGING" }
+        val nyTask = task.copy(id = 0L, versjon = 0L, triggerTid = LocalDateTime.now().plusMinutes(15))
+
+        taskService.save(task.copy(payload = "${task.payload}-klonetTilNy"))
+        secureLogger.info(nyTask.toString())
+        val lagretTask = taskService.save(nyTask)
+
+        logger.info("Kloner task med id ${task.id}. Opprettet ny task: ${lagretTask.id}")
+        return lagretTask
+    }
+}
 
 @RestController
 @RequestMapping(
@@ -24,38 +45,17 @@ import kotlin.reflect.full.findAnnotation
 @ProtectedWithClaims(issuer = "azuread")
 class TaskForvaltningsController(
     private val taskService: TaskService,
+    private val taskForvaltningService: TaskForvaltningService,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
     @PostMapping("/restart/{taskId}")
-    @Transactional
     fun hentStatus(
         @PathVariable taskId: Long,
     ): ResponseEntity<String> {
         logger.info("Starter kloning av task id $taskId.")
-
         val task = taskService.findById(taskId)
-
-        check(task.status == Status.MANUELL_OPPFØLGING) { "Task må ha status MANUELL_OPPFØLGING" }
-        logger.info("Fant task: ${task.id} med type ${task.type}.")
-
-        val annotation = task::class.findAnnotation<TaskStepBeskrivelse>()
-        val maxAntallFeil = annotation?.maxAntallFeil ?: 3
-        val antallGangerPlukket = taskService.antallGangerPlukket(taskId)
-
-        logger.info("Tasken har blitt plukket $antallGangerPlukket ganger. Maks antall feil tillatt: $maxAntallFeil.")
-
-        check(antallGangerPlukket > maxAntallFeil) { "Tasken kan ikke plukkes på nytt, da den ikke er plukket for mange ganger: $antallGangerPlukket" }
-
-        val nyTask = task.copy(id = 0L, triggerTid = LocalDateTime.now().plusMinutes(15))
-
-        secureLogger.info(nyTask.toString())
-
-        taskService.save(task.copy(payload = task.payload + "-konet"))
-        val lagretTask = taskService.save(nyTask) // For å restarte tasken, må vi sette id til 0, slik at den blir opprettet på nytt
-
-        logger.info("Kloner task med id ${task.id}. Opprettet ny task: ${lagretTask.id}")
-        return ResponseEntity("OK - opprettet ${lagretTask.id}", HttpStatus.OK)
+        val lagretTask = taskForvaltningService.kopierTask(task)
+        return ResponseEntity("OK - opprettet ${lagretTask.id}, fra ${task.id}", HttpStatus.OK)
     }
 }
---
+++ src/test/kotlin/no/nav/familie/ef/iverksett/infrastruktur/task/TaskForvaltningServiceTest.kt
@@ -0,0 +1,69 @@
+package no.nav.familie.ef.iverksett.infrastruktur.task
+
+import jakarta.annotation.PostConstruct
+import no.nav.familie.ef.iverksett.ServerTest
+import no.nav.familie.ef.iverksett.brev.DistribuerVedtaksbrevTask
+import no.nav.familie.prosessering.domene.Status
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.internal.TaskService
+import no.nav.familie.prosessering.internal.TaskWorker
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDateTime
+import java.util.*
+
+class TaskForvaltningServiceTest : ServerTest() {
+    @Autowired
+    private lateinit var taskService: TaskService
+
+    @Autowired
+    private lateinit var taskForvaltningService: TaskForvaltningService
+
+    @Autowired
+    private lateinit var taskWorker: TaskWorker
+
+    var taskId: Long = 0
+
+    val payload = UUID.randomUUID().toString()
+
+    @PostConstruct
+    fun init() {
+        val savedTask = taskService.save(lagTask())
+        assertThat(savedTask.status).isEqualTo(Status.UBEHANDLET)
+        taskId = savedTask.id
+        kjørTask(taskId)
+    }
+
+    @Test
+    internal fun `Klon eksisterende task`() {
+        val task = taskService.findById(taskId)
+        assertThat(taskService.antallGangerPlukket(task.id)).isEqualTo(DistribuerVedtaksbrevTask.MAX_FORSØK)
+
+        val kopi = taskForvaltningService.kopierTask(task)
+
+        val orginalTaskFraDb = taskService.findById(taskId)
+        assertThat(orginalTaskFraDb.payload).isEqualTo(payload + "-klonetTilNy")
+        assertThat(taskService.antallGangerPlukket(kopi.id)).isEqualTo(0)
+        assertThat(kopi.payload).isEqualTo(payload)
+        assertThat(kopi.versjon).isEqualTo(1)
+    }
+
+    private fun lagTask(): Task = Task(type = DistribuerVedtaksbrevTask.TYPE, payload = this.payload, status = Status.UBEHANDLET).medTriggerTid(LocalDateTime.now().minusDays(1))
+
+    private fun kjørTask(taskId: Long) {
+        taskService.findById(taskId)
+        repeat(52, { doWork(taskId) })
+    }
+
+    fun doWork(taskId: Long) {
+        try {
+            taskWorker.markerPlukket(taskId)
+            println("Nytt forsøk")
+            taskWorker.doActualWork(taskId)
+        } catch (e: IllegalStateException) {
+            // forventet å få feil her
+            taskWorker.doFeilhåndtering(taskId, e)
+        }
+    }
+}
