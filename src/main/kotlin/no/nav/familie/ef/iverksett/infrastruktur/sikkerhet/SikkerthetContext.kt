554d10ed1aee47ea8ffb016e1ebffebb45383dab
krikof-nav <kristian.kofoed@nav.no>
2025-12-09 10:20:57 +0100

    Begynner å utbedre "kallKommerFra" implementason som tar høyde for kall fra ef-sak og familie-prosessering. WIP.

---
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/sikkerhet/SikkerthetContext.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.ef.iverksett.infrastruktur.sikkerhet
+
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
+import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
+
+object SikkerthetContext {
+    private val logger = LoggerFactory.getLogger(SikkerthetContext::class.java)
+    private val teamLogsMarker = MarkerFactory.getMarker("TEAM_LOGS")
+
+    fun kallKommerFraEfSak(): Boolean = kallKommerFra("teamfamilie:familie-ef-sak")
+    fun kallKommerFraFraProsessering(): Boolean = kallKommerFra("teamfamilie:familie-prosessering")
+
+    private fun kallKommerFra(forventetApplikasjonsSuffix: String): Boolean {
+        val claims = SpringTokenValidationContextHolder().getTokenValidationContext().getClaims("azuread")
+
+        val applikasjonsnavn = claims.get("azp_name")?.toString() ?: "" // e.g. dev-gcp:some-team:application-name
+
+        logger.info(teamLogsMarker, "Applikasjonsnavn: $applikasjonsnavn")
+        return applikasjonsnavn.endsWith(forventetApplikasjonsSuffix)
+    }
+}
