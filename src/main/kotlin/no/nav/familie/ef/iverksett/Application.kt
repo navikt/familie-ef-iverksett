aab6e93c74195e85a59f0f0a5bd433a83bfbf652
krikof-nav <kristian.kofoed@nav.no>
2025-12-09 13:21:07 +0100

    Legger til start up logging for debugging, skal fjernes på sikt.

--- src/main/kotlin/no/nav/familie/ef/iverksett/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/Application.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.ef.iverksett
 
+import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -7,5 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
+    // TODO: Dette skal fjernes, kun for å forsikre oss om at loggin i Dev fungerer.
+    val logger = LoggerFactory.getLogger(Application::class.java)
+    val teamLogsMarker = MarkerFactory.getMarker("TEAM_LOGS")
+
+    logger.info("Denne skal logge til GCP logs UTEN TeamMarker i Dev")
+    logger.info(teamLogsMarker, "Denne skal logge til GCP logs MED TeamMarker i Dev")
+
     SpringApplication.run(Application::class.java, *args)
 }
