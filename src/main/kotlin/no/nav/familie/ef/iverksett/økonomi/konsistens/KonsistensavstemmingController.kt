587e70cd0834776585eba7fffdae2703477dfbbc
krikof-nav <kristian.kofoed@nav.no>
2025-12-09 13:11:38 +0100

    Tester med teamLogsMarker.

--- src/main/kotlin/no/nav/familie/ef/iverksett/økonomi/konsistens/KonsistensavstemmingController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/økonomi/konsistens/KonsistensavstemmingController.kt
@@ -5,6 +5,7 @@ import no.nav.familie.ef.iverksett.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.kontrakter.ef.iverksett.KonsistensavstemmingDto
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
@@ -24,6 +25,7 @@ class KonsistensavstemmingController(
     private val konsistensavstemmingService: KonsistensavstemmingService,
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private val teamLogsMarker = MarkerFactory.getMarker("TEAM_LOGS")
 
     // TODO må verifisere at man ikke lagrer ned kildeBehandlingId i iverksett
     // Og må verifisere at man ikke bruker kildeBehandlingId på andelsnivå fra KonsistensavstemmingDto
@@ -35,7 +37,7 @@ class KonsistensavstemmingController(
         @RequestParam(name = "transaksjonId") transaksjonId: UUID? = null,
     ) {
         if (!SikkerhetContext.kallKommerFraEfSak()) {
-            logger.error("Kall kommer ikke fra ef-sak")
+            logger.error(teamLogsMarker, "Kall kommer ikke fra ef-sak")
             throw ApiFeil("Kall kommer ikke fra ef-sak", HttpStatus.FORBIDDEN)
         }
 
@@ -52,7 +54,7 @@ class KonsistensavstemmingController(
         @RequestParam(name = "sekunder") sekunder: Long,
     ): String {
         if (!SikkerhetContext.kallKommerFraEfSak() && !SikkerhetContext.kallKommerFraFraProsessering()) {
-            logger.error("Kall kommer ikke fra ef-sak eller familie-prosessering")
+            logger.error(teamLogsMarker, "Kall kommer ikke fra ef-sak eller familie-prosessering")
             throw ApiFeil("Kall kommer ikke fra ef-sak eller familie-prosessering", HttpStatus.FORBIDDEN)
         }
 
