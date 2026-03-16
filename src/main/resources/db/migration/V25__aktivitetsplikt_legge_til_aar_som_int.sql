23bd183f4b239c94c43e5028f41482dc8be9ea27
endrehm <endre.heien.mo@nav.no>
2026-03-16 13:56:42 +0100

    Vente med å gjøre aar ikke nullable for å ikke kræsje tester

--- src/main/resources/db/migration/V25__aktivitetsplikt_legge_til_aar_som_int.sql
+++ src/main/resources/db/migration/V25__aktivitetsplikt_legge_til_aar_som_int.sql
@@ -1,3 +1,2 @@
 ALTER TABLE aktivitetsplikt_brev ADD COLUMN ar INT;
 UPDATE aktivitetsplikt_brev SET ar = EXTRACT(YEAR FROM gjeldende_ar)::INT;
-ALTER TABLE aktivitetsplikt_brev ALTER COLUMN ar SET NOT NULL;
