9fe20853e4f3c22b3c1a55fadfa6b0dc1bd03176
endrehm <endre.heien.mo@nav.no>
2026-05-07 12:32:20 +0200

    La til en siste sjekk for å få mer info om inlogget bruker

--- src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/sikkerhet/TestControllerSlettMeg.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/infrastruktur/sikkerhet/TestControllerSlettMeg.kt
@@ -3,6 +3,8 @@ package no.nav.familie.ef.iverksett.infrastruktur.sikkerhet
 import no.nav.familie.kontrakter.felles.Ressurs
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -35,5 +37,14 @@ class TestControllerSlettMeg {
     fun testFraEfSak(): ResponseEntity<Ressurs<String>> {
         SikkerhetContext.validerKallKommerFraEfSak()
         return ResponseEntity.ok(Ressurs.success("OK - kall kom fra ef-sak"))
+    }
+
+    @GetMapping("/hvem-er-du")
+    @PreAuthorize("hasRole('BESLUTTER') or hasRole('SAKSBEHANDLER') or hasRole('APPLICATION')")
+    fun hvemErDu(): ResponseEntity<Ressurs<String>> {
+        val jwt = SecurityContextHolder.getContext().authentication as JwtAuthenticationToken
+        val azpName = jwt.token.claims["azp_name"] as? String ?: "ukjent"
+        val roles = jwt.authorities.map { it.authority }
+        return ResponseEntity.ok(Ressurs.success("azp_name=$azpName, roller=$roles"))
     }
 }
