f52eda08be79c6fff52a362bf9ee5393abb5812d
EndreHM <endre.heien.mo@nav.no>
2026-05-27 10:36:59 +0200

    Legger til saksbehandeler rolle på preauth frittståendebrev (#1166)

--- src/main/kotlin/no/nav/familie/ef/iverksett/brev/BrevController.kt
+++ src/main/kotlin/no/nav/familie/ef/iverksett/brev/BrevController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/api/brev"])
-@PreAuthorize("hasRole('APPLICATION')")
+@PreAuthorize("hasRole('APPLICATION') or hasRole('SAKSBEHANDLER')")
 class BrevController(
     private val frittståendeBrevService: FrittståendeBrevService,
 ) {
